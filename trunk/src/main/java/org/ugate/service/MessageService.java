acb1c06a93d48836f4b527f5910196a443d53f1b
ugate.relay@gmail.com <ugate.relay@gmail.com@18fea87c-ecab-a400-3ebc-87fcf074c283>
2012-04-28 12:58:32 +0000

    Migration to H2 DB back-end (not complete). Hopefully, an embedded web option will be available via TomEE. For now, just migrating to stand-alone Apache OpenJPA over H2 DB. Added web server (not complete- need to get JTA impl)

---
+++ trunk/src/main/java/org/ugate/service/MessageService.java
@@ -0,0 +1,10 @@
+package org.ugate.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageService {
+	
+	private static final Logger log = LoggerFactory.getLogger(MessageService.class);
+
+}
