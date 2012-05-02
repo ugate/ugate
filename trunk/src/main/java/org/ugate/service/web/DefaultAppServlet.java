8e0695a06a6431eb8837f604d0530ce46be06c48
ugate.relay@gmail.com <ugate.relay@gmail.com@18fea87c-ecab-a400-3ebc-87fcf074c283>
2012-05-02 19:05:21 +0000

    Migration to H2 DB back-end (not complete). Migrating to stand-alone Apache OpenJPA over H2 DB using Atomikos JTA. Working, but flush is not occurring after commit via OpenJPA/Atomikos.

--- trunk/src/main/java/org/ugate/service/web/DefaultAppServlet.java
+++ trunk/src/main/java/org/ugate/service/web/DefaultAppServlet.java
@@ -1,41 +1,41 @@
 package org.ugate.service.web;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.util.List;
 
-import javax.naming.Binding;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
 import javax.persistence.EntityManager;
-import javax.persistence.LockModeType;
 import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
-import org.apache.openjpa.persistence.OpenJPAEntityManager;
-import org.apache.openjpa.persistence.OpenJPAPersistence;
-import org.eclipse.jetty.plus.jndi.Transaction;
+import org.eclipse.jetty.plus.servlet.ServletHandler;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.ugate.UGateUtil;
+import org.ugate.service.ServiceManager;
 import org.ugate.service.entity.jpa.Message;
 
-public class DefaultHandler extends AbstractHandler {
+public class DefaultAppServlet extends DefaultServlet {
 
-	private static final Logger log = LoggerFactory.getLogger(DefaultHandler.class);
+	private static final Logger log = LoggerFactory.getLogger(DefaultAppServlet.class);
+	
+	public DefaultAppServlet() {
+		super();
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(null, null, request, response);
+	}
 
-	public void handle(final String target, final Request baseRequest, final HttpServletRequest request, 
+	//@Override
+	public void doHandle(final String target, final Request baseRequest, final HttpServletRequest request, 
 			final HttpServletResponse response) throws IOException, ServletException {
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
-		baseRequest.setHandled(true);
+		//baseRequest.setHandled(true);
 		response.getWriter().println("<h1>Hello UGate World</h1>");
 		try {
 	        // Create a new EntityManagerFactory using the System properties.
@@ -48,18 +48,11 @@ public class DefaultHandler extends AbstractHandler {
 	        // EntityManager is the main object in the persistence API, and is
 	        // used to create, delete, and query objects, as well as access
 	        // the current transaction
-	        EntityManager em = WebServer.getEmFactory().createEntityManager();
-	        OpenJPAEntityManager oem = OpenJPAPersistence.cast(em);
-	        Connection conn = (Connection) oem.getConnection();
-	        boolean autoCommit = conn.getAutoCommit();
+	        EntityManager em = ServiceManager.getEmFactory().createEntityManager();
 	        
-	        try {
-	        	// http://stackoverflow.com/questions/2131798/jetty-mysql-connection-pool-configuration-error-javax-naming-namenotfoundexcept
-	        	final Context ic = (Context) new InitialContext().lookup("java:comp");
-	    		listContext(ic, "");
-	        } catch (Throwable t) {
-	        	log.info("JNDI lookup error: ", t);
-	        }
+//	        OpenJPAEntityManager oem = OpenJPAPersistence.cast(em);
+//	        Connection conn = (Connection) oem.getConnection();
+//	        boolean autoCommit = conn.getAutoCommit();
 
 	        // Begin a new local transaction so that we can persist a new entity
 //	        em.getTransaction().begin();
@@ -79,7 +72,7 @@ public class DefaultHandler extends AbstractHandler {
 	        em.close();
 
 	        // Create a fresh, new EntityManager
-	        EntityManager em2 = WebServer.getEmFactory().createEntityManager();
+	        EntityManager em2 = ServiceManager.getEmFactory().createEntityManager();
 
 	        // Perform a simple query for all the Message entities
 	        Query q = em2.createQuery("select m from Message m");
@@ -98,23 +91,4 @@ public class DefaultHandler extends AbstractHandler {
 			log.error("JPA error: ", t);
 		}
 	}
-
-	private static final void listContext(Context ctx, String indent) {
-	    try {
-	        NamingEnumeration<?> list = ctx.listBindings("");
-	        while (list.hasMore()) {
-	            Binding item = (Binding) list.next();
-	            String className = item.getClassName();
-	            String name = item.getName();
-	            System.out.println(indent + className + " " + name);
-	            Object o = item.getObject();
-	            if (o instanceof javax.naming.Context) {
-	                listContext((Context) o, indent + " ");
-	            }
-	        }
-	    } catch (NamingException ex) {
-	        System.out.println(ex);
-	    }
-	}
-
 }
---
+++ trunk/src/main/java/org/ugate/service/web/TransactionFilter.java
@@ -0,0 +1,89 @@
+package org.ugate.service.web;
+
+import javax.naming.Binding;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
+public class TransactionFilter implements Filter {
+	// Use the Atomikos UserTransaction implementation to start and end
+	// the transactions!
+	//private J2eeUserTransaction utx = new J2eeUserTransaction();
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+		try {
+
+			// start a new transaction for this request
+			//utx.begin();
+			// http://stackoverflow.com/questions/2131798/jetty-mysql-connection-pool-configuration-error-javax-naming-namenotfoundexcept
+			getTransaction().setTransactionTimeout(1000);
+			getTransaction().begin();
+
+			// delegate the request to the next filter, and eventually to the
+			// target servlet or JSP
+			chain.doFilter(request, response);
+
+			// if no exception happened: commit the transaction
+			//utx.commit();
+			getTransaction().commit();
+		} catch (final Throwable t) {
+			// analyze exception to dermine of rollback is required or not
+			// and then call rollback or commit on utx as appropriate
+			t.printStackTrace();
+			try {
+				getTransaction().rollback();
+			} catch (final Throwable t2) {
+			}
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		try {
+			listContext(new InitialContext(), "");
+		} catch (final NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected TransactionManager getTransaction() {
+		try {
+			return (TransactionManager) new InitialContext().lookup("UserTransaction");
+		} catch (final NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	protected static final void listContext(Context ctx, String indent) {
+	    try {
+	        NamingEnumeration<?> list = ctx.listBindings("");
+	        while (list.hasMore()) {
+	            Binding item = (Binding) list.next();
+	            String className = item.getClassName();
+	            String name = item.getName();
+	            System.out.println(indent + className + " " + name);
+	            Object o = item.getObject();
+	            if (o instanceof javax.naming.Context) {
+	                listContext((Context) o, indent + " ");
+	            }
+	        }
+	    } catch (NamingException ex) {
+	        System.out.println(ex);
+	    }
+	}
+}
