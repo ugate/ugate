0b452ccd4e8bddb70524cecd9c9af7f5395d4a24
ugate.relay <ugate.relay@gmail.com>
2012-05-08 14:12:06 +0000

    Migration to H2 DB back-end (not complete). Fixed maven build issues. Added JavaFX preloader. Cleaned up web server filters/servlets.

--- src/main/java/org/ugate/service/web/GlobalFilter.java
+++ src/main/java/org/ugate/service/web/GlobalFilter.java
@@ -1,92 +1,62 @@
 package org.ugate.service.web;
 
-import javax.naming.Binding;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.transaction.TransactionManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.ugate.resources.RS;
+
+/**
+ * {@linkplain Filter} to wrap common output for {@linkplain Servlet}s
+ */
 @WebFilter
 public class GlobalFilter implements Filter {
+
+	private static final Logger log = LoggerFactory.getLogger(GlobalFilter.class);
 	
-	// Use the Atomikos UserTransaction implementation to start and end
-	// the transactions!
-	//private J2eeUserTransaction utx = new J2eeUserTransaction();
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) {
 		try {
+			if (log.isDebugEnabled()) {
+				log.debug(String.format("Entering filter from remote address %1$s", 
+						request.getRemoteAddr()));
+			}
+			response.setContentType("text/html;charset=utf-8");
+			response.getWriter().println("<html><body>");
+			response.getWriter().println("<h1>" + RS.rbLabel("app.title") + "</h1>");
 
-			// start a new transaction for this request
-			//utx.begin();
-			// http://stackoverflow.com/questions/2131798/jetty-mysql-connection-pool-configuration-error-javax-naming-namenotfoundexcept
-//			getTransaction().setTransactionTimeout(1000);
-//			getTransaction().begin();
-
-			// delegate the request to the next filter, and eventually to the
-			// target servlet or JSP
+			// delegate the request to the next filter, and eventually to the target servlet or JSP
 			chain.doFilter(request, response);
 
-			// if no exception happened: commit the transaction
-			//utx.commit();
-//			getTransaction().commit();
+			response.getWriter().println("</body></html>");
 		} catch (final Throwable t) {
-			// analyze exception to dermine of rollback is required or not
-			// and then call rollback or commit on utx as appropriate
-			t.printStackTrace();
-			try {
-				getTransaction().rollback();
-			} catch (final Throwable t2) {
-			}
+			log.error("Unable to process " + GlobalFilter.class.getSimpleName(), t);
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void destroy() {
-
+		//
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		try {
-			listContext(new InitialContext(), "");
-		} catch (final NamingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	protected TransactionManager getTransaction() {
-		try {
-			return (TransactionManager) new InitialContext().lookup("UserTransaction");
-		} catch (final NamingException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	protected static final void listContext(Context ctx, String indent) {
-	    try {
-	        NamingEnumeration<?> list = ctx.listBindings("");
-	        while (list.hasMore()) {
-	            Binding item = (Binding) list.next();
-	            String className = item.getClassName();
-	            String name = item.getName();
-	            System.out.println(indent + className + " " + name);
-	            Object o = item.getObject();
-	            if (o instanceof javax.naming.Context) {
-	                listContext((Context) o, indent + " ");
-	            }
-	        }
-	    } catch (NamingException ex) {
-	        System.out.println(ex);
-	    }
+		// 
 	}
 }
