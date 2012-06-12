22fa0de447052e7686322e411efd6444ac2bbe67
ugate.relay@gmail.com <ugate.relay@gmail.com@18fea87c-ecab-a400-3ebc-87fcf074c283>
2012-06-12 00:10:51 +0000

    Migration to H2 DB back-end (not complete). Updated BeanPathAdaptor with temporary example application- still may need additional type handling on FieldProperty#set(Object). Still getting error when running Maven build (not getting from eclipse run): java.lang.NoClassDefFoundError: Could not initialize class gnu.io.CommPortIdentifier (due to RXTX installation and only thrown when RXTX is not previously installed)

--- trunk/src/main/java/org/ugate/gui/components/BeanPathAdaptorTest.java
+++ trunk/src/main/java/org/ugate/gui/components/BeanPathAdaptorTest.java
@@ -1,6 +1,7 @@
 package org.ugate.gui.components;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.value.ChangeListener;
@@ -14,6 +15,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -35,20 +37,14 @@ public class BeanPathAdaptorTest extends Application {
 		"ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH",
 		"OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI",
 		"WV","WY"};
-	private BeanPathAdaptor<Person> person1 = new BeanPathAdaptor<Person>(new Person());
-	private BeanPathAdaptor<Person> person2 = new BeanPathAdaptor<Person>(new Person());
+	private static final String P1_LABEL = "Person 1";
+	private static final String P2_LABEL = "Person 2";
+	private final Person person1 = new Person();
+	private final Person person2 = new Person();
+	private final BeanPathAdaptor<Person> personPA = new BeanPathAdaptor<Person>(person1);
 	
 	public static void main(final String[] args) {
 		Application.launch(BeanPathAdaptorTest.class, args);
-		final Person person = new Person();
-		final BeanPathAdaptor<Person> personBA = new BeanPathAdaptor<Person>(person);
-		final TextField tf = new TextField("Nothing");
-		personBA.bindBidirectional("address.street", tf.textProperty());
-		System.out.println("POJO Street: " + person.getAddress().getStreet());
-		System.out.println("Adaptor Street: " + tf.getText());
-		tf.setText("Something");
-		System.out.println("POJO Street: " + person.getAddress().getStreet());
-		System.out.println("Adaptor Street: " + tf.getText());
 	}
 
 	@Override
@@ -58,24 +54,25 @@ public class BeanPathAdaptorTest extends Application {
 		pojoTA.setWrapText(true);
 		pojoTA.setEditable(false);
 		pBox = new ChoiceBox<>(FXCollections.observableArrayList(
-				"Person 1", "Person 2"));
+				P1_LABEL, P2_LABEL));
 		pBox.getSelectionModel().select(0);
+		pBox.valueProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable,
+					String oldValue, String newValue) {
+				personPA.setBean(newValue == P1_LABEL ? person1 : person2);
+			}
+		});
 		pBox.autosize();
 		ToolBar toolBar = new ToolBar();
 		toolBar.getItems().add(pBox);
 		VBox personBox = new VBox(10);
 		personBox.setPadding(new Insets(10, 10, 10, 50));
 		FlowPane beanPane = new FlowPane();
-		Button dumpPojoBtn = new Button("Dump Person POJO");
-		dumpPojoBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				dumpPojo(getSelectedAdaptor());
-			}
-		});
 		personBox.getChildren().addAll(
 				new Text("Person POJO using auto-generated JavaFX properties:"), 
-				beanTF("name", 50, null, "[a-zA-z0-9\\s]*"), 
+				beanTF("name", 50, null, "[a-zA-z0-9\\s]*"),
+				beanTF("age", 100, Slider.class, null),
 				beanTF("address.street", 50, null, "[a-zA-z0-9\\s]*"),
 				beanTF("address.location.state", 2, ChoiceBox.class, 
 						"[a-zA-z]", STATES),
@@ -85,15 +82,29 @@ public class BeanPathAdaptorTest extends Application {
 				new Label("POJO Dump:"),
 				pojoTA);
 		beanPane.getChildren().addAll(personBox);
+
+		final TextField pojoNameTF = new TextField();
+		Button pojoNameBtn = new Button("Set Person's Name");
+		pojoNameBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				personPA.getBean().setName(pojoNameTF.getText());
+				dumpPojo(personPA);
+			}
+		});
+		VBox pojoBox = new VBox(10);
+		pojoBox.setPadding(new Insets(10, 10, 10, 10));
+		pojoBox.getChildren().addAll(new Label("Set person's name via POJO:"),
+				pojoNameTF, pojoNameBtn);
 		
 		VBox beanBox = new VBox(10);
 		beanBox.getChildren().addAll(toolBar, beanPane);
 		SplitPane root = new SplitPane();
-		root.getItems().addAll(beanBox);
+		root.getItems().addAll(beanBox, pojoBox);
 		primaryStage.setOnShowing(new EventHandler<WindowEvent>() {
 			@Override
 			public void handle(WindowEvent event) {
-				dumpPojo(getSelectedAdaptor());
+				dumpPojo(personPA);
 			}
 		});
 		primaryStage.setScene(new Scene(root));
@@ -101,22 +112,29 @@ public class BeanPathAdaptorTest extends Application {
 	}
 	
 	@SafeVarargs
-	public final void dumpPojo(BeanPathAdaptor<Person>... ps) {
-		String dump = "";
-		for (BeanPathAdaptor<Person> p : ps) {
-			dump += "Person 1 {name=" + 
-					p.getBean().getName() +
-					", address.street=" +
-					p.getBean().getAddress().getStreet() +
-					", address.location.state=" + 
-					p.getBean().getAddress().getLocation().getState() +
-					", address.location.country=" +
-					p.getBean().getAddress().getLocation().getCountry() +
-					", address.location.international=" +
-					p.getBean().getAddress().getLocation().isInternational() +
-					"}\n";
-		}
-		pojoTA.setText(dump);
+	public final void dumpPojo(final BeanPathAdaptor<Person>... ps) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				String dump = "";
+				for (BeanPathAdaptor<Person> p : ps) {
+					dump += "Person 1 {name=" + 
+							p.getBean().getName() +
+							", age=" +
+							p.getBean().getAge() +
+							", address.street=" +
+							p.getBean().getAddress().getStreet() +
+							", address.location.state=" + 
+							p.getBean().getAddress().getLocation().getState() +
+							", address.location.country=" +
+							p.getBean().getAddress().getLocation().getCountry() +
+							", address.location.international=" +
+							p.getBean().getAddress().getLocation().isInternational() +
+							"}\n";
+				}
+				pojoTA.setText(dump);
+			}
+		});
 	}
 	
 	public <T> HBox beanTF(String path, final int maxChars, 
@@ -124,7 +142,6 @@ public class BeanPathAdaptorTest extends Application {
 			final String restictTo, 
 			@SuppressWarnings("unchecked") T... choices) {
 		HBox box = new HBox();
-		final BeanPathAdaptor<Person> bpa = getSelectedAdaptor();
 		Control ctrl;
 		if (controlType == CheckBox.class) {
 			CheckBox cb = new CheckBox();
@@ -132,11 +149,11 @@ public class BeanPathAdaptorTest extends Application {
 				@Override
 				public void changed(ObservableValue<? extends Boolean> observable,
 						Boolean oldValue, Boolean newValue) {
-					dumpPojo(bpa);
+					dumpPojo(personPA);
 				}
 			});
 			// POJO binding magic...
-			bpa.bindBidirectional(path, cb.selectedProperty());
+			personPA.bindBidirectional(path, cb.selectedProperty());
 			ctrl = cb;
 		} else if (controlType == ChoiceBox.class) {
 			ChoiceBox<T> cb = new ChoiceBox<>(
@@ -144,12 +161,30 @@ public class BeanPathAdaptorTest extends Application {
 			cb.valueProperty().addListener(new InvalidationListener() {
 				@Override
 				public void invalidated(Observable observable) {
-					dumpPojo(bpa);
+					dumpPojo(personPA);
 				}
 			});
 			// POJO binding magic...
-			bpa.bindBidirectional(path, cb.valueProperty());
+			personPA.bindBidirectional(path, cb.valueProperty());
 			ctrl = cb;
+		} else if (controlType == Slider.class) {
+			Slider sl = new Slider();
+			sl.setShowTickLabels(true);
+			sl.setShowTickMarks(true);
+			sl.setMajorTickUnit(maxChars/2);
+			sl.setMinorTickCount(7);
+			sl.setBlockIncrement(1);
+			sl.setMax(maxChars+1);
+			sl.setSnapToTicks(true);
+			sl.valueProperty().addListener(new InvalidationListener() {
+				@Override
+				public void invalidated(Observable observable) {
+					dumpPojo(personPA);
+				}
+			});
+			// POJO binding magic...
+			personPA.bindBidirectional(path, sl.valueProperty());
+			ctrl = sl;
 		} else {
 			final TextField tf = new TextField() {
 				@Override 
@@ -173,21 +208,15 @@ public class BeanPathAdaptorTest extends Application {
 				@Override
 				public void changed(ObservableValue<? extends String> observable,
 						String oldValue, String newValue) {
-					dumpPojo(bpa);
+					dumpPojo(personPA);
 				}
 			});
 			// POJO binding magic...
-			bpa.bindBidirectional(path, tf.textProperty());
+			personPA.bindBidirectional(path, tf.textProperty());
 			ctrl = tf;
 		}
 		box.getChildren().addAll(new Label(path + " = "), ctrl);
 		return box;
-	}
-	
-	public BeanPathAdaptor<Person> getSelectedAdaptor() {
-		return pBox.getSelectionModel() == null || 
-				pBox.getSelectionModel().getSelectedIndex() <= 0 ? 
-				person1 : person2;
 	}
 	
 	public HBox beanTFW(String startLabel, String endLabel, TextField... tfs) {
@@ -201,6 +230,7 @@ public class BeanPathAdaptorTest extends Application {
 	public static class Person {
 		private String name;
 		private Address address;
+		private double age;
 		public String getName() {
 			return name;
 		}
@@ -212,6 +242,12 @@ public class BeanPathAdaptorTest extends Application {
 		}
 		public void setAddress(Address address) {
 			this.address = address;
+		}
+		public double getAge() {
+			return age;
+		}
+		public void setAge(double age) {
+			this.age = age;
 		}
 	}
 	public static class Address {
